b8496eb74895049b2c432e2c4b9397924c2baab7
Jacqueline Lee <anginblu@gmail.com>
2018-01-09 06:56:43 +0000

    automatically backed up by learn

--- db/migrate/20180109063854_create_users.rb
+++ db/migrate/20180109063854_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.decimal :balance
+      t.decimal :balance, :default => 0
     end
   end
 end
